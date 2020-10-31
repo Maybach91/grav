6c10cca0550e036b20381aba60d04716a800fffd
Patrick Ludewig <yes@patrick.works>
2020-10-31 08:41:16 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,4 @@
 <?php
- phpinfo();
 /**
  * @package    Grav.Core
  *
@@ -12,6 +11,7 @@ namespace Grav;
 \define('GRAV_REQUEST_TIME', microtime(true));
 \define('GRAV_PHP_MIN', '7.1.3');
 
+ phpinfo();
 if (version_compare($ver = PHP_VERSION, $req = GRAV_PHP_MIN, '<')) {
     die(sprintf('You are running PHP %s, but Grav needs at least <strong>PHP %s</strong> to run.', $ver, $req));
 }
